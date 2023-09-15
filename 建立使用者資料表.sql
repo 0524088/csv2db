daa1df4cd7595847ed392937a2ede29d07cd43c2
0524088 <43835584+0524088@users.noreply.github.com>
2023-09-15 11:46:15 +0800

    新增 建立使用者資料表.sql

---
+++ 建立使用者資料表.sql
@@ -0,0 +1,9 @@
+-- 建立使用者資料表
+CREATE TABLE IF NOT EXISTS `uesrs` (
+	id int AUTO_INCREMENT PRIMARY KEY,
+    account varchar(80),
+    password varchar(80),
+    token varchar(80),
+    created_at datetime,
+    updated_at datetime
+)
